b14926848017b765b610281df74a302bd2c4d36f
Ali pakdaman <alipakdaman68@gmail.com>
2022-06-22 17:01:22 +0430

    second example

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+const miles2kilometes = 1.6
+
 func main() {
-	fmt.Println("hello!")
+	var miles float64
+	fmt.Print("enter miles:")
+	_, err := fmt.Scanf("%v", &miles)
+	if err != nil {
+		fmt.Println(err)
+	}
+	kilomete := miles2kilometes * miles
+	fmt.Println(miles, " is equal to ", kilomete)
 }
